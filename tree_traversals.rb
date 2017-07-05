a0f5b676289841594b544b83d1f8c3f6a644f384
Jonathon Wright <jonwright93@gmail.com>
2017-07-04 20:33:22 -0700

    implement in-order traversal iterative

--- tree_traversals.rb
+++ tree_traversals.rb
@@ -1,22 +1,64 @@
-def in_order(root)
-  return nil unless root
+class TreeNode
+  attr_accessor :data, :left, :right
 
-    in_order(root.left)
-    p root
-    in_order(root.right)
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
 end
 
-def pre_order(root)
-  return nil unless root
-  p root
-  pre_order(root.left)
-  pre_order(root.right)
+# def in_order(root)
+#   return nil unless root
+#
+#     in_order(root.left)
+#     p root
+#     in_order(root.right)
+# end
+#
+# def pre_order(root)
+#   return nil unless root
+#   p root
+#   pre_order(root.left)
+#   pre_order(root.right)
+#
+# end
+#
+# def post_order(root)
+#   return nil unless root
+#   post_order(root.left)
+#   post_order(root.right)
+#   p root
+# end
 
+
+def in_order_iterative(root)
+  stack = []
+  result = []
+  current = root
+  done = false
+
+  until done == true
+    if current
+      stack << current
+      current = current.left
+    else
+      if stack.empty?
+        done = true
+      else
+        current = stack.pop
+        result << current.data
+        current = current.right
+      end
+    end
+  end
+  result
 end
 
-def post_order(root)
-  return nil unless root
-  post_order(root.left)
-  post_order(root.right)
-  p root
-end
+root = TreeNode.new(1)
+left = root.left = TreeNode.new(2)
+right = root.right = TreeNode.new(3)
+left.left = TreeNode.new(4)
+left.right = TreeNode.new(5)
+
+puts in_order_iterative(root)
