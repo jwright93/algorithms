6ab3da564c5a11483846af4e6e1cfb84241f44d2
Jonathon Wright <jonwright93@gmail.com>
2017-07-05 12:25:43 -0700

    implement merge two binary trees

---
+++ trees.rb
@@ -0,0 +1,22 @@
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val)
+        @data = val
+        @left, @right = nil, nil
+    end
+end
+
+def merge_tree(t1, t2)
+  return nil if t1 == nil && t2 == nil
+
+  if t1 && t2
+    root = TreeNode.new(t1.data + t2.data)
+    root.left = merge_tree(t1.left, t2.left)
+    root.right = merge_tree(t1.right, t2.right)
+    root
+  else
+    t1 || t2
+  end
+
+
+end
