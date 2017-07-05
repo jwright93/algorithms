49d5b92fd2156e9388947958d59b7de8a82927e6
Jonathon Wright <jonwright93@gmail.com>
2017-07-05 12:43:12 -0700

    implement find_compliment

--- random.rb
+++ random.rb
@@ -47,10 +47,21 @@ end
 
 def hamming_distance(x, y)
   answer = 0
-  until x == 0 || y == 0
+  until x == 0 && y == 0
     answer += 1 if (x % 2) & (y % 2)
     x /= 2
     y /= 2
   end
   answer
+end
+
+
+def number_compliment(num)
+  answer = ''
+  until num == 0
+    num % 2 == 0 ? answer += '1' : answer += '0'
+    num /=2
+  end
+
+  answer.to_i(2)
 end
