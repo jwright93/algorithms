5dc6e3eda2ff3b53aa5f4460cfbc174091312fff
Jonathon Wright <jonwright93@gmail.com>
2017-07-05 20:42:58 -0700

    implement 1.1 - 1.5

---
+++ ctci.rb
@@ -0,0 +1,76 @@
+require 'set'
+# 1.1
+
+def unique_characters(word)
+  # 0(n) time complexity o(n) space complexity
+  # collected = Set.new
+  # i = 0
+  # until i == word.length
+  #   return false if collected.include?(word[i])
+  #   collected << word[i]
+  #   i += 1
+  # end
+  #
+  # true
+
+  # 0(nlogn) time complexity o(1) space complexity
+
+  letters = word.dup.chars.sort
+
+  i = 0
+
+  until i == letters.length - 1
+    return false if letters[i] == letters[i + 1]
+    i += 1
+  end
+
+  true
+
+end
+
+
+def string_permutation(str1, str2)
+  # 0(nlogn) time complexity o(1) space complexity
+  # return false if str1.length != str2.length
+  #
+  # str1.chars.sort == str2.chars.sort
+  #
+  #
+
+  return false if str1.length != str2.length
+  count1 = Hash.new(0)
+  count2 = Hash.new(0)
+  # 0(n) time complexity o(n) space complexity
+
+  str1.chars.each do |letter|
+    count1[letter] += 1
+  end
+
+  str2.chars.each do |letter|
+    count2[letter] += 1
+  end
+
+  count1 == count2
+
+end
+
+def string_compression(string)
+  result = ''
+  count = 1
+  i = 1
+
+  until i == string.length
+    if string[i] == string[i - 1]
+      count+= 1
+    else
+      result += "#{string[i-1]}#{count}"
+      count = 1
+    end
+    i += 1
+  end
+  result += "#{string[string.length - 1]}#{count}"
+  result.length <= string.length ? result : string
+end
+
+
+## 2.2
