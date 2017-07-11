53c54fa7eb18a1e83455ee2f9d2df2d80a80a92d
Jonathon Wright <jonwright93@gmail.com>
2017-07-11 15:17:31 -0700

    ctci 1.5

--- ctci_6th.rb
+++ ctci_6th.rb
@@ -13,4 +13,49 @@ def palindrome_permutation(string)
 end
 
 
-puts palindrome_permutation("Tact Coa")
+#1.5
+
+def one_away(str1, str2)
+  return true if str1 == str2
+
+  case str1.length <=> str2.length
+  when 1
+    insert(str1, str2)
+  when 0
+    replace(str1, str2)
+  when -1
+    insert(str2, str1)
+  end
+end
+
+def replace(str1, str2)
+  count = 0
+  i = 0
+  until i == str1.length
+     count += 1 if str1[i] != str2[i]
+     return false if count > 1
+    i += 1
+  end
+  true
+end
+
+def insert(str1, str2)
+  i = 0
+
+  until i == str1.length
+    if str1[i] != str2[i]
+      str2 = str2[0...i] + str1[i] + str2[i..-1]
+    end
+
+    if str1 == str2
+      return true
+    else
+      return false
+    end
+  end
+  false
+end
+
+# puts one_away('bale', 'kale')
+# puts one_away('ale', 'kale')
+# puts one_away('ale', 'kald')
