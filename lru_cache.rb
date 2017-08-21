e61473e51652fcd74e918764c62f1a84a2ab5c71
Jonathon Wright <jonwright93@gmail.com>
2017-08-20 23:02:55 -0700

    update lru cache set method

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,4 +1,5 @@
-class LRU_Cache
+class LRU
+  attr_accessor :head, :tail, :count, :table
   def initialize(max_items = 10)
       @max_items = max_items
       @head = nil
@@ -9,32 +10,53 @@ class LRU_Cache
 
   def set(key, value)
     @count += 1
-    @head = @head.next if @count > max_items
+    @head = @head.next if @count > @max_items
 
     new_node = Node.new(value)
 
-    @head = new_node if tail == nil
+    @head = new_node if @tail == nil
     @tail.next = new_node if @tail != nil
     @tail  = new_node
-    @table[key] = value
+    @table[key] = new_node
   end
 
   def get(key)
-    # res = @table[key]
-    # return res if res.next == nil
-    #
-    # if res.previous != nil
-    #   r
-    # end
+    res = @table[key]
+    return nil if res == nil
+    return res if res.next == nil
+    if res.previous != nil
+      res.previous.next = res.next
+      res.next.previous = res.previous
+    end
+    @tail.next = res
+    @tail = res
+    res
   end
 end
 
 
 class Node
+  attr_accessor :value, :previous, :next
+
   def initialize(value = nil)
-    attr_accessor :value, :prevous, :next
     @value = value
     @previous = nil
     @next = nil
   end
 end
+
+
+lru = LRU.new(3)
+lru.set('a', 1)
+lru.set('b', 2)
+lru.set('c', 3)
+puts lru.head.value
+lru.get('a')
+puts lru.head.value
+lru.get('b')
+puts lru.head.value
+lru.get('c')
+puts lru.head.value
+lru.set('d', 8)
+lru.set('e', 9)
+puts lru.head.value
